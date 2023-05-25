4df6d6cbd2a948d098538f561fa9d968a5efc563
burulai <burulaiurbaeva7@gmail.com>
2023-05-25 23:24:20 +0600

    changed

--- main.py
+++ main.py
@@ -5,37 +5,43 @@ import random
 # Initialize the game
 pygame.init()
 pygame.font.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((800, 430))
 pygame.display.set_caption('Wolf')
 clock = pygame.time.Clock()
 test_fount = pygame.font.Font(None, 50)
 
+back_img = pygame.image.load('img/game-wrap-bg.png')
+back_img = pygame.transform.scale(back_img, screen.get_size())
+screen.blit(back_img, (0,0))
+
 # Background
 main_img = pygame.image.load('img/game-bg.jpg').convert()
+
 # Wolf
 wolf1_img = pygame.image.load('img/wolf/wolf-p-0.png').convert_alpha()
 wolf2_img = pygame.image.load('img/wolf/wolf-p-1.png').convert_alpha()
 wolf1_rect = wolf1_img.get_rect(topleft=(178, 167))
 wolf2_rect = wolf2_img.get_rect(topleft=(298, 167))
 wolf1_x = 125
-wolf1_y = 155
-score = 0
-# Button
-button_img = pygame.image.load('img/arrow-keys.png').convert() #  (650,250)
+wolf2_x = 350
 
-# Text
-score_surf = test_fount.render('Wolf game', False, 'Black').convert()
-score_rect = score_surf.get_rect(center=(400,50))
+score = 0
+# # Button
+# button_img = pygame.image.load('img/arrow-keys.png').convert()  # (650,250)
 
 # Logo image
-logo_img = pygame.image.load('img/logo.png').convert()
-
+logo_img = pygame.image.load('img/logo.png').convert_alpha()
+# logo_img = pygame.transform.scale(logo_img, (150,200))
+screen.blit(logo_img, (150, 350))
 # Font
 font = pygame.font.Font(None, 36)
 
-# Button
-nav_button = pygame.image.load('img/nav-button.png').convert_alpha()
-restart_icon = pygame.image.load('img/restart-icon.png')
+# # Button
+# nav_button = pygame.image.load('img/nav-button.png').convert_alpha()
+# screen.blit(nav_button, (500, 370))
+# restart_icon = pygame.image.load('img/restart-icon.png').convert_alpha()
+# restart_icon = pygame.transform.scale(restart_icon, (30,30))
+# screen.blit(restart_icon, (430,370))
 
 # Basket
 basket1u = pygame.image.load('img/basket/basket-p-0-0.png').convert_alpha()
@@ -45,18 +51,11 @@ basket2d = pygame.image.load('img/basket/basket-p-1-1.png')
 
 # Egg properties
 egg_img = pygame.image.load('img/egg/egg.png').convert_alpha()
-egg_positions = [[50,120], [50,200], [500,120], [500,200]]
+egg_positions = [[50, 120], [50, 200], [500, 120], [500, 200]]
 egg_speed = 3
 random_egg = random.choice(egg_positions)
 egg_x_position = random_egg[0]
 egg_y_position = random_egg[1]
-
-egg_rect = egg_img.get_rect(topleft = (100, 100))
-
-player_gravity = 0
-
-game_over = False
-
 
 def check_collision():
     global score, egg_x_position, egg_y_position, random_egg
@@ -74,6 +73,7 @@ def check_collision():
     egg_x_position = random_egg[0]
     egg_y_position = random_egg[1]
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,7 +81,29 @@ while True:
             exit()
 
     # Background
-    screen.blit(main_img, (0,0))
+    screen.blit(main_img, (0, 0))
+
+    screen.blit(egg_img, (egg_x_position, egg_y_position))
+
+    # Wolf keys
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        screen.blit(wolf1_img, wolf1_rect)
+    if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+        screen.blit(wolf1_img, wolf1_rect)
+        screen.blit(basket1d, (125, 155))
+    if keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+        screen.blit(wolf1_img, wolf1_rect)
+        screen.blit(basket1u, (125, 235))
+    if keys[pygame.K_RIGHT]:
+        screen.blit(wolf2_img, wolf2_rect)
+    if keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+        screen.blit(wolf2_img, wolf2_rect)
+        screen.blit(basket2d, (367, 162))
+    if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+        screen.blit(wolf2_img, wolf2_rect)
+        screen.blit(basket2u, (362, 240))
+
     if random_egg[0] == 50:
         egg_x_position += 1
         egg_y_position += 1
@@ -106,28 +128,14 @@ while True:
         egg_y_position = random_egg[1]
         check_collision()
 
-    screen.blit(egg_img, (egg_x_position,egg_y_position))
-
-    if egg_x_position==wolf1_x:
+    if egg_x_position == wolf1_x and keys[pygame.K_LEFT] and keys[pygame.K_UP]:
         score += 1
-    # Wolf keys
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        screen.blit(wolf1_img, wolf1_rect)
-    if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
-        screen.blit(wolf1_img, wolf1_rect)
-        screen.blit(basket1d, (125, 155))
-    if keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
-        screen.blit(wolf1_img, wolf1_rect)
-        screen.blit(basket1u, (125, 235))
-    if keys[pygame.K_RIGHT]:
-        screen.blit(wolf2_img, wolf2_rect)
-    if keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
-        screen.blit(wolf2_img, wolf2_rect)
-        screen.blit(basket2d, (367, 162))
-    if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
-        screen.blit(wolf2_img, wolf2_rect)
-        screen.blit(basket2u, (362, 240))
+    if egg_x_position == wolf1_x and keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+        score += 1
+    if egg_x_position == wolf2_x and keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+        score += 1
+    if egg_x_position == wolf2_x and keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+        score += 1
 
     # Display the score
     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
@@ -135,5 +143,3 @@ while True:
 
     pygame.display.update()
     clock.tick(50)
-
-
